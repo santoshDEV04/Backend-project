b57a2f94efd3a7449fd13d30de3a353b5c09ecd8
Santosh Kumar Dash <dashsantosh1333@gmail.com>
2026-01-04 09:15:26 +0530

    cludinary and multer setup done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+
+    const options = {
+        resource_type: auto,
+    }
+
+    try {
+        if(!localFilePath) return null;
+        //upload the file on Cloudinary
+        const result = await cloudinary.uploader.upload(localFilePath, options);
+
+        //file has been uploadded successfully
+
+        console.log("file is uploaded on cloudinary", result.url)
+        return result;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temperory file as the upload operation got faild
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+
+
+
+
+cloudinary.uploader.upload()
