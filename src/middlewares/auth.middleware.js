bab0e61ad66d63506ce496313740448f698dd4dc
Santosh Kumar Dash <dashsantosh1333@gmail.com>
2026-01-06 19:24:26 +0530

    add refreshAccessToken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
-import jwt,{ JsonWebTokenError } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 
 
